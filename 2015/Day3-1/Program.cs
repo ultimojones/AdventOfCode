5fc767aab236fdfea5a994940554408db3fa4a85
Bert Lyons <ultimojones@gmail.com>
2022-12-14 08:21:05 +1100

    🤠

---
+++ 2015/Day3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+var santa = new Point(0, 0);
+var visits = new List<Point>() { santa };
+
+var directions = File.ReadAllText("input.txt");
+foreach (var dir in directions)
+{
+    santa.Offset(dir switch
+    {
+        '^' => new Point(0, -1),
+        'v' => new Point(0, +1),
+        '<' => new Point(-1, 0),
+        '>' => new Point(+1, 0),
+        _ => default
+    });
+    visits.Add(santa);
+}
+
+Console.WriteLine(visits.Distinct().Count());
+
---
+++ 2015/Day3-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+var santa = new Point(0, 0);
+var robos = new Point(0, 0);
+var visits = new List<Point>() { santa, robos };
+
+var dirs = File.ReadAllText("input.txt");
+for (int i = 0; i < dirs.Length; i++)
+{
+    var offset = dirs[i] switch
+    {
+        '^' => new Point(0, -1),
+        'v' => new Point(0, +1),
+        '<' => new Point(-1, 0),
+        '>' => new Point(+1, 0),
+        _ => default
+    };
+    if (i % 2 == 0)
+    {
+        santa.Offset(offset);
+        visits.Add(santa);
+    }
+    else
+    {
+        robos.Offset(offset);
+        visits.Add(robos);
+    }
+}
+
+Console.WriteLine(visits.Distinct().Count());
+
