742bba92132625aaad458ee7e9a0c5e0b6028738
Bert Lyons <ultimojones@gmail.com>
2022-12-19 15:03:05 +1100

    🤠

--- 2022/Day12/Program.cs
+++ 2022/Day12/Program.cs
@@ -1,124 +1,102 @@
-﻿using System.Drawing;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
-namespace Day12
+var grid = File.ReadLines("input.txt").SelectMany((line, y) => line.Select((letter, x) =>
+    new { x, y, letter })).ToDictionary(g => (X: g.x, Y: g.y),
+    g => (Letter: g.letter, Height: (int)g.letter switch { 'S' => 'a', 'E' => 'z', _ => g.letter }));
+
+var start = grid.First(g => g.Value.Letter == 'S');
+var end = grid.First(g => g.Value.Letter == 'E');
+var gridMinX = grid.Min(g => g.Key.X);
+var gridMinY = grid.Min(g => g.Key.Y);
+var gridMaxX = grid.Max(g => g.Key.X);
+var gridMaxY = grid.Max(g => g.Key.Y);
+
+var selectedRoutes = new ConcurrentDictionary<(int X, int Y), (int X, int Y)[]>();
+var routes = new List<(int X, int Y)[]>();
+routes.Add(new[] { start.Key });
+
+while (routes.Count > 0 && !selectedRoutes.ContainsKey(end.Key))
 {
-    internal class Program
+    var nextRoutes = routes.AsParallel().SelectMany(r =>
     {
-        static void Main(string[] args)
+        var current = r.Last();
+        var nexts = new (int X, int Y)[]
         {
-            Part1();
-        }
+            (current.X - 1, current.Y),
+            (current.X + 1, current.Y),
+            (current.X, current.Y - 1),
+            (current.X, current.Y + 1)
+        };
 
-        private static void Part1()
-        {
-            var lines = File.ReadAllLines("input.txt");
-            int maxX = lines[0].Length;
-            int maxY = lines.Length;
+        return nexts.Where(n => n.X >= gridMinX && n.X <= gridMaxX && n.Y >= gridMinY && n.Y <= gridMaxY
+            && grid[n].Height - grid[current].Height <= 1)
+        .Select(n => new { n, r = r.Append(n).ToArray() })
+        .Where(r => selectedRoutes.TryAdd(r.n, r.r))
+        .Select(r => r.r);
+    });
+    routes = nextRoutes.ToList();
+}
 
-            var grid = new Dictionary<(int X, int Y), int>();
-            var height = Enumerable.Range(0, maxX).SelectMany(x => Enumerable.Range(0, maxY).Select(y => (x, y)))
-                .ToDictionary(i => i, i => lines[i.y][i.x]);
-            var start = height.First(h => h.Value == 'S').Key;
-            var end = height.First(h => h.Value == 'E').Key;
-            height[start] = 'a';
-            height[end] = 'z';
+var final = selectedRoutes[end.Key];
 
-            grid[start] = 0;
-            var nextSteps = GetNeighbours(start).ToList();
-
-            for (int i = 0; i < 1000; i++)
+PrintRoute(final);
+Console.WriteLine(
+    );
+void PrintRoute((int X, int Y)[] final)
+{
+    for (int y = 0; y <= gridMaxY; y++)
+    {
+        var line = new string(Enumerable.Range(gridMinX, gridMaxX - gridMinX + 1)
+            .Select(x =>
             {
-                var checking = nextSteps.Distinct().ToArray();
-                nextSteps.Clear();
-
-                foreach (var step in checking)
+                var pos = (x, y);
+                if (end.Key == pos)
                 {
-                    var neighbours = GetNeighbours(step).ToArray();
-                    var numSteps = neighbours.Where(n => height[n] - height[step] >= -1 && grid.ContainsKey(n)).Select(n => grid[n]).Min() + 1;
-                    grid[step] = numSteps;
-                    if (step == end) { Console.WriteLine($"Steps to end = {numSteps}"); break; }
-                    nextSteps.AddRange(neighbours.Where(n => !grid.ContainsKey(n) && height[n] - height[step] <= 1));
+                    return 'E';
                 }
-            }
-
-            IEnumerable<(int X, int Y)> GetNeighbours((int X, int Y) cur) => 
-                new (int X, int Y)[] { (cur.X - 1, cur.Y), (cur.X + 1, cur.Y), (cur.X, cur.Y - 1), (cur.X, cur.Y + 1) }
-                .Where(n => n.X >= 0 && n.X < maxX && n.Y >= 0 && n.Y < maxY);
-}
+                else if (final.Contains(pos))
+                {
+                    var index = Array.FindIndex(final, f => f == pos);
+                    if (index == final.Length - 1)
+                    {
+                        return 'X';
+                    }
+                    else
+                    {
+                        var nextPos = final[index + 1];
+                        switch (nextPos.X - pos.x, nextPos.Y - pos.y)
+                        {
+                            case (0, -1):
+                                return '^';
+                                break;
+                            case (0, 1):
+                                return 'v';
+                                break;
+                            case (-1, 0):
+                                return '<';
+                                break;
+                            case (1, 0):
+                                return '>';
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                return '.';
+            }).ToArray());
+        Console.WriteLine(line);
     }
-
 }
 
-        //    private static void Part1()
-        //    {
-        //        (int X, int Y) start = default, end = default;
-        //        var lines = File.ReadAllLines("sample.txt");
-        //        int maxx = lines[0].Length;
-        //        int maxy = lines.Length;
-
-//        var square = new Square { X = start.X, Y = start.Y, Z = 'a' };
-//        var squares = new Dictionary<(int X, int Y), Square>();
-//        var next = new List<(int X, int Y)>();
-
-//        next.AddRange(
-//            new (int X, int Y)[]
-//            {
-//                (square.X - 1, square.Y),
-//                (square.X + 1, square.Y),
-//                (square.X, square.Y - 1),
-//                (square.X, square.Y + 1),
-//            }.Where(n => n.X >= 0 && n.X < maxx && n.Y >= 0 && n.Y < maxy
-//                    && !squares.ContainsKey(n)
-//                    && lines[n.X][n.Y] - square.Z <= 1)
-//        );
-
-//        for (int i = 0; i < 100; i++)
-//        {
-//            var current = next.Distinct().ToArray();
-//            next.Clear();
-
-//            foreach (var c in current)
-//            {
-//                var steps =
-//                    new (int X, int Y)[]
-//                    {
-//                        (c.X - 1, c.Y),
-//                        (c.X + 1, c.Y),
-//                        (c.X, c.Y - 1),
-//                        (c.X, c.Y + 1),
-//                    }.Select(d => squares[d]?.Steps).Min() + 1;
-//                squares[c] = new Square { X = c.X, Y = c.Y, Z = lines[c.X][c.Y], Steps = steps.Value };
-
-//                next.AddRange(
-//                    new (int X, int Y)[]
-//                    {
-//                        (c.X - 1, c.Y),
-//                        (c.X + 1, c.Y),
-//                        (c.X, c.Y - 1),
-//                        (c.X, c.Y + 1),
-//                    }.Where(n => n.X >= 0 && n.X < maxx && n.Y >= 0 && n.Y < maxy
-//                            && !squares.ContainsKey(n)
-//                            && lines[n.X][n.Y] - square.Z <= 1));
-//            }
-//        }
-//    }
-
-//}
-
-//class Square
-//{
-//    public int X { get; set; }
-//    public int Y { get; set; }
-//    public int Z { get; set; }
-//    public int Steps { get; set; }
-//}
-//var grid = new char[lines[0].Length, lines.Length];
-//for (int i = 0; i < lines.Length; i++)
-//{
-//    for (int j = 0; j < lines[i].Length; j++)
-//    {
-//        grid[j, i] = lines[i][j];
-//        if (grid[j, i] == 'S') start = (j, i);
-//        if (grid[j, i] == 'E') end = (j, i);
-//    }
-//}
+while (true)
+{
+    var rng = new Random();
+    var randRoute = selectedRoutes.OrderBy(x => rng.Next()).First();
+    PrintRoute(randRoute.Value);
+    Console.WriteLine("Press any key...");
+    Console.ReadKey(true);
+}
