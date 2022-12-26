ffc40bfa85f108af4343a05857fcb18ee9d9bff8
Bert Lyons <ultimojones@gmail.com>
2022-12-26 13:38:28 +1100

    🤠

---
+++ 2022/Day20-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+var items = File.ReadLines("input.txt").Select((x, p) => (p, x: int.Parse(x))).ToArray();
+var list = new LinkedList<(int p, int x)>(items);
+var len = items.Length;
+
+foreach (var item in items)
+{
+    Console.WriteLine(item);
+    var cur = list.Find(item)!;
+    for (int i = 0; i < Math.Abs(cur.Value.x); i++)
+    {
+        LinkedListNode<(int, int)> next;
+        if (cur.Value.x > 0)
+        {
+            next = cur.Next ?? list.First!;
+        }
+        else
+        {
+            next = cur.Previous?.Previous ?? list.Last!;
+        }
+        list.Remove(cur);
+        list.AddAfter(next, cur);
+    }
+    //Console.WriteLine(string.Join(", ", list.Select(x => x.x)));
+}
+
+var pos = list.TakeWhile(x => x.x != 0).Count();
+var calc1000 = list.ElementAt((pos + 1000) % len).x;
+var calc2000 = list.ElementAt((pos + 2000) % len).x;
+var calc3000 = list.ElementAt((pos + 3000) % len).x;
+Console.WriteLine(calc1000 + calc2000 + calc3000);
---
+++ 2022/Day20-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+var items = File.ReadLines("sample.txt").Select((x, p) => (p, x: long.Parse(x) * 811589153)).ToArray();
+var list = new LinkedList<(int p, long x)>(items);
+var len = items.Length;
+
+for (int l = 0; l < 10; l++)
+{
+    foreach (var item in items)
+    {
+        //Console.WriteLine(item);
+        var cur = list.Find(item)!;
+        for (int i = 0; i < Math.Abs(cur.Value.x) % (len - 1); i++)
+        {
+            LinkedListNode<(int, long)> next;
+            if (cur.Value.x > 0)
+            {
+                next = cur.Next ?? list.First!;
+            }
+            else
+            {
+                next = cur.Previous?.Previous ?? list.Last!;
+            }
+            list.Remove(cur);
+            list.AddAfter(next, cur);
+        }
+    }
+    Console.WriteLine(string.Join(", ", list.Select(x => x.x)));
+}
+
+var pos = list.TakeWhile(x => x.x != 0).Count();
+var calc1000 = list.ElementAt((pos + 1000) % len).x;
+var calc2000 = list.ElementAt((pos + 2000) % len).x;
+var calc3000 = list.ElementAt((pos + 3000) % len).x;
+Console.WriteLine(calc1000 + calc2000 + calc3000);
