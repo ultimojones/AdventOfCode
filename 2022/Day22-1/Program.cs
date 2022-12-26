3dc892c19485e1aaf2992f2c7157015685ef1b10
Bert Lyons <ultimojones@gmail.com>
2022-12-27 08:46:54 +1100

    🤠

--- 2022/Day22-1/Program.cs
+++ 2022/Day22-1/Program.cs
@@ -1,5 +1,5 @@
 ﻿var grid = new Dictionary<(int X, int Y), char>();
-var file = File.OpenText("sample.txt");
+var file = File.OpenText("input.txt");
 string? line = file.ReadLine();
 for (int y = 1; !string.IsNullOrEmpty(line); y++, line = file.ReadLine())
 {
@@ -33,45 +33,77 @@ var pos = grid.Keys.Where(k => k.Y == 1).MinBy(k => k.X);
 var dir = '>';
 grid[pos] = dir;
 
-foreach (var cmd in cmds.Take(1))
+try
 {
-    if (cmd.Move.HasValue)
+    foreach (var cmd in cmds)
     {
-        if (dir == '>')
+        if (cmd.Move.HasValue)
         {
-            var axis = grid.Where(g => g.Key.Y == pos.Y);
-            var min = axis.Min(g => g.Key.X);
-            var max = axis.Max(g => g.Key.X);
-            var range = max - min + 1;
-            var start = pos;
-            for (int i = 1, x = start.X; i < cmd.Move.Value ; i++)
+            if (dir == '>' || dir == '<')
             {
-                x = (x + 1 - min) % range + min;
-                if (grid[(x, pos.Y)] == '#') break;
+                var axis = grid.Where(g => g.Key.Y == pos.Y);
+                var min = axis.Min(g => g.Key.X);
+                var max = axis.Max(g => g.Key.X);
+                var range = max - min + 1;
+                var start = pos;
+                var offset = dir == '>' ? 1 : -1;
+                for (int i = 0, x = start.X; i < cmd.Move.Value; i++)
+                {
+                    x = x == min && offset == -1 ? max
+                      : x == max && offset == 1 ? min
+                      : x + offset;
+                    if (grid[(x, pos.Y)] == '#') break;
 
-                pos.X = x;
-                grid[pos] = dir;
+                    pos.X = x;
+                    grid[pos] = dir;
+                }
+            }
+            else if (dir == 'v' || dir == '^')
+            {
+                var axis = grid.Where(g => g.Key.X == pos.X);
+                var min = axis.Min(g => g.Key.Y);
+                var max = axis.Max(g => g.Key.Y);
+                var range = max - min + 1;
+                var start = pos;
+                var offset = dir == 'v' ? 1 : -1;
+                for (int i = 0, y = start.Y; i < cmd.Move.Value; i++)
+                {
+                    y = y == min && offset == -1 ? max
+                      : y == max && offset == 1 ? min
+                      : y + offset;
+                    if (grid[(pos.X, y)] == '#') break;
+
+                    pos.Y = y;
+                    grid[pos] = dir;
+                }
             }
         }
-    }
-    else
-    {
-        grid[pos] = dir = (cmd.Turn, dir) switch
+        else
         {
-            ('R', '>') => 'v',
-            ('R', 'v') => '<',
-            ('R', '<') => '^',
-            ('R', '^') => '>',
-            ('L', '>') => '^',
-            ('L', 'v') => '>',
-            ('L', '<') => 'v',
-            ('L', '^') => '<',
-            _ => throw new NotImplementedException()
-        };
+            grid[pos] = dir = (cmd.Turn, dir) switch
+            {
+                ('R', '>') => 'v',
+                ('R', 'v') => '<',
+                ('R', '<') => '^',
+                ('R', '^') => '>',
+                ('L', '>') => '^',
+                ('L', 'v') => '>',
+                ('L', '<') => 'v',
+                ('L', '^') => '<',
+                _ => throw new NotImplementedException()
+            };
+        }
     }
-}
 
-PrintGrid();
+    PrintGrid();
+    Console.WriteLine(1000 * pos.Y + 4 * pos.X + dir switch { '>' => 0, 'v' => 1, '<' => 2, '^' => 3 });
+
+}
+catch (Exception ex)
+{
+    PrintGrid();
+    throw;
+}
 
 
 void PrintGrid()
