d0aca12683b0c3cc9669c3a065e971e4b6f4ff3e
Bert Lyons <ultimojones@gmail.com>
2022-12-27 19:29:56 +1100

    🤠

---
+++ 2022/Day23-1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Concurrent;
+
+var grid = new Dictionary<(int X, int Y), (int X, int Y)>();
+int n = 0;
+foreach (var line in File.ReadLines("input.txt"))
+{
+    n++;
+    for (int x = 0; x < line.Length; x++)
+    {
+        if (line[x] == '#')
+            grid[(x, n)] = default;
+    }
+}
+
+int moves = 0, rounds = 0;
+for (int i = 0; ; i++)
+{
+    foreach (var elf in grid)
+    {
+        if (grid.ContainsKey(NW(elf.Key))
+         || grid.ContainsKey(N(elf.Key))
+         || grid.ContainsKey(NE(elf.Key))
+         || grid.ContainsKey(E(elf.Key))
+         || grid.ContainsKey(SE(elf.Key))
+         || grid.ContainsKey(S(elf.Key))
+         || grid.ContainsKey(SW(elf.Key))
+         || grid.ContainsKey(W(elf.Key)))
+        {
+            for (int check = i % 4, test = 0; test < 4; test++, check = (check + 1) % 4)
+            {
+                if (check == 0)
+                {
+                    if (!grid.ContainsKey(NW(elf.Key))
+                     && !grid.ContainsKey(N(elf.Key))
+                     && !grid.ContainsKey(NE(elf.Key)))
+                    {
+                        grid[elf.Key] = N(elf.Key);
+                        break;
+                    }
+                }
+                else if (check == 1)
+                {
+                    if (!grid.ContainsKey(SW(elf.Key))
+                     && !grid.ContainsKey(S(elf.Key))
+                     && !grid.ContainsKey(SE(elf.Key)))
+                    {
+                        grid[elf.Key] = S(elf.Key);
+                        break;
+                    }
+                }
+                else if (check == 2)
+                {
+                    if (!grid.ContainsKey(NW(elf.Key))
+                     && !grid.ContainsKey(W(elf.Key))
+                     && !grid.ContainsKey(SW(elf.Key)))
+                    {
+                        grid[elf.Key] = W(elf.Key);
+                        break;
+                    }
+                }
+                else if (check == 3)
+                {
+                    if (!grid.ContainsKey(NE(elf.Key))
+                     && !grid.ContainsKey(E(elf.Key))
+                     && !grid.ContainsKey(SE(elf.Key)))
+                    {
+                        grid[elf.Key] = E(elf.Key);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    moves = 0;
+    foreach (var elf in grid.ToArray())
+    {
+        if (grid.Values.Count(v => v == elf.Value) == 1)
+        {
+            grid[elf.Value] = default;
+            grid.Remove(elf.Key);
+            moves++;
+        }
+    }
+
+    Console.WriteLine($"{i}={moves}");
+
+    if (moves == 0)
+    {
+        rounds = i + 1;
+        break;
+    }
+
+}
+
+var minX = grid.Min(g => g.Key.X);
+var maxX = grid.Max(g => g.Key.X);
+var minY = grid.Min(g => g.Key.Y);
+var maxY = grid.Max(g => g.Key.Y);
+
+for (int y = minY; y < maxY + 1; y++)
+{
+    Console.WriteLine(new string(Enumerable.Range(minX, maxX - minX + 1).Select(x => grid.ContainsKey((x, y)) ? '#' : '.').ToArray()));
+}
+Console.WriteLine();
+
+var endMinX = grid.Min(g => g.Key.X);
+var endMaxX = grid.Max(g => g.Key.X);
+var endMinY = grid.Min(g => g.Key.Y);
+var endMaxY = grid.Max(g => g.Key.Y);
+
+Console.WriteLine((endMaxX - endMinX + 1) * (endMaxY - endMinY + 1) - grid.Count);
+Console.WriteLine();
+Console.WriteLine($"After {rounds} rounds.");
+
+
+(int X, int Y) NW((int X, int Y) point) => (point.X - 1, point.Y - 1);
+(int X, int Y) N((int X, int Y) point) => (point.X, point.Y - 1);
+(int X, int Y) NE((int X, int Y) point) => (point.X + 1, point.Y - 1);
+(int X, int Y) E((int X, int Y) point) => (point.X + 1, point.Y);
+(int X, int Y) SE((int X, int Y) point) => (point.X + 1, point.Y + 1);
+(int X, int Y) S((int X, int Y) point) => (point.X, point.Y + 1);
+(int X, int Y) SW((int X, int Y) point) => (point.X - 1, point.Y + 1);
+(int X, int Y) W((int X, int Y) point) => (point.X - 1, point.Y);
---
+++ 2022/Day23-2/Program.cs
@@ -0,0 +1,108 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+var grid = new List<(int X, int Y)>();
+
+var lines = File.ReadAllLines("input.txt");
+for (int i = 0, y = 1; i < lines.Length; i++, y++)
+{
+    var line = lines[i];
+    for (int j = 0, x = 1; j < line.Length; j++, x++)
+    {
+        if (line[j] == '#')
+        {
+            grid.Add((x, y));
+        }
+    }
+}
+
+var completed = 0;
+for (int i = 0; ; i++)
+{
+    List<((int X, int Y) Cur, (int X, int Y) Next)> next = grid.AsParallel().Select(elf =>
+    {
+        var near = new[] { NW(elf), N(elf), NE(elf), E(elf), SE(elf), S(elf), SW(elf), W(elf) };
+        if (near.Intersect(grid).Any())
+        {
+            for (int j = 0, check = i % 4; j < 4; j++, check = (check + 1) % 4)
+            {
+                if (check == 0)
+                {
+                    if (!near[0..3].Intersect(grid).Any())
+                    {
+                        return (elf, near[1]);
+                    }
+                }
+                else if (check == 1)
+                {
+                    if (!near[4..7].Intersect(grid).Any())
+                    {
+                        return (elf, near[5]);
+                    }
+                }
+                else if (check == 2)
+                {
+                    if (!new[] { near[0], near[6], near[7] }.Intersect(grid).Any())
+                    {
+                        return (elf, near[7]);
+                    }
+                }
+                else if (check == 3)
+                {
+                    if (!near[2..5].Intersect(grid).Any())
+                    {
+                        return (elf, near[3]);
+                    }
+                }
+            }
+        }
+        return (elf, elf);
+    }).ToList();
+
+    int moved = 0;
+    grid = next.GroupBy(k => k.Next).SelectMany(g =>
+    {
+        var first = g.First();
+        if (g.Count() == 1)
+        {
+            if (first.Cur != first.Next)
+                moved++;
+            return new[] { first.Next };
+        }
+        else
+        {
+            moved += g.Count();
+            return g.Select(p => p.Cur);
+        }
+    }).ToList();
+
+    if (moved == 0)
+    {
+        completed = i + 1;
+        break;
+    }
+    Console.WriteLine($"{i + 1}={moved}");
+}
+
+var minX = grid.Min(g => g.X);
+var maxX = grid.Max(g => g.X);
+var minY = grid.Min(g => g.Y);
+var maxY = grid.Max(g => g.Y);
+
+for (int y = minY; y < maxY + 1; y++)
+{
+    Console.WriteLine(new string(Enumerable.Range(minX, maxX - minX + 1).Select(x => grid.Contains((x, y)) ? '#' : '.').ToArray()));
+}
+Console.WriteLine();
+
+//Console.WriteLine((maxX - minX + 1) * (maxY - minY + 1) - grid.Count);
+Console.WriteLine(completed);
+
+
+(int X, int Y) NW((int X, int Y) point) => (point.X - 1, point.Y - 1);
+(int X, int Y) N((int X, int Y) point) => (point.X, point.Y - 1);
+(int X, int Y) NE((int X, int Y) point) => (point.X + 1, point.Y - 1);
+(int X, int Y) E((int X, int Y) point) => (point.X + 1, point.Y);
+(int X, int Y) SE((int X, int Y) point) => (point.X + 1, point.Y + 1);
+(int X, int Y) S((int X, int Y) point) => (point.X, point.Y + 1);
+(int X, int Y) SW((int X, int Y) point) => (point.X - 1, point.Y + 1);
+(int X, int Y) W((int X, int Y) point) => (point.X - 1, point.Y);
