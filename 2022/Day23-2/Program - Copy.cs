b78454444d2c9eab24e065949a8fee470952893c
Bert Lyons <ultimojones@gmail.com>
2022-12-27 22:04:04 +1100

    🤠

--- 2022/Day23-2/Program - Copy.cs
+++ 2022/Day23-2/Program - Copy.cs
@@ -1,6 +1,6 @@
 ﻿using static System.Net.Mime.MediaTypeNames;
 
-var grid = new List<(int X, int Y)>();
+var grid = new HashSet<(int X, int Y)>();
 
 var lines = File.ReadAllLines("input.txt");
 for (int i = 0, y = 1; i < lines.Length; i++, y++)
@@ -16,40 +16,40 @@ for (int i = 0, y = 1; i < lines.Length; i++, y++)
 }
 
 var completed = 0;
-for (int i = 0; i < 10 ; i++)
+for (int i = 0; ; i++)
 {
     int moved = 0;
     grid = grid.AsParallel().Select(elf =>
     {
         var near = new[] { NW(elf), N(elf), NE(elf), E(elf), SE(elf), S(elf), SW(elf), W(elf) };
-        if (near.Any(n => grid.Contains(n)))
+        if (near.Intersect(grid).Any())
         {
             for (int j = 0, check = i % 4; j < 4; j++, check = (check + 1) % 4)
             {
                 if (check == 0)
                 {
-                    if (!near[0..3].Any(n => grid.Contains(n)))
+                    if (!near[0..3].Intersect(grid).Any())
                     {
                         return (elf, near[1]);
                     }
                 }
                 else if (check == 1)
                 {
-                    if (!near[4..7].Any(n => grid.Contains(n)))
+                    if (!near[4..7].Intersect(grid).Any())
                     {
                         return (elf, near[5]);
                     }
                 }
                 else if (check == 2)
                 {
-                    if (!new[] { near[0], near[6], near[7] }.Any(n => grid.Contains(n)))
+                    if (!new[] { near[0], near[6], near[7] }.Intersect(grid).Any())
                     {
                         return (elf, near[7]);
                     }
                 }
                 else if (check == 3)
                 {
-                    if (!near[2..5].Any(n => grid.Contains(n)))
+                    if (!near[2..5].Intersect(grid).Any())
                     {
                         return (elf, near[3]);
                     }
@@ -71,7 +71,7 @@ for (int i = 0; i < 10 ; i++)
             moved += g.Count();
             return g.Select(p => p.Item1);
         }
-    }).ToList();
+    }).ToHashSet();
 
     if (moved == 0)
     {
---
+++ 2022/Day24-1/Program.cs
@@ -0,0 +1,78 @@
+﻿var border = new List<(int X, int Y)>();
+var blizzards = new List<(int X, int Y, char Dir)>();
+var lines = File.ReadAllLines("sample.txt");
+for (int y = 0; y < lines.Length; y++)
+{
+	for (int x = 0; x < lines[y].Length; x++)
+	{
+		switch (lines[y][x])
+		{
+			case '#':
+				border.Add((x, y));
+                break;
+			case '<' or '>' or '^' or 'v':
+				blizzards.Add((x, y, lines[y][x]));
+                break;
+            default:
+				break;
+		}
+	}
+}
+var maxX = border.Max(b => b.X);
+var maxY = border.Max(b => b.Y);
+var cur = (1, 0);
+
+var bestRoute = "";
+var bestTime = 0;
+
+
+
+
+
+blizzards = blizzards.Select(b => b.Dir switch
+{
+	'>' => (b.X == maxX - 1 ? 1 : b.X + 1, b.Y, b.Dir),
+	'<' => (b.X == 1 ? maxX - 1 : b.X - 1, b.Y, b.Dir),
+	'^' => (b.X, b.Y == 1 ? maxY - 1 : b.Y - 1, b.Dir),
+	'v' => (b.X, b.Y == maxY - 1 ? 1 : b.Y + 1, b.Dir),
+	_ => throw new NotImplementedException()
+}).ToList();
+
+
+
+PrintGrid();
+
+void PrintGrid()
+{
+	for (int y = 0; y < maxY + 1; y++)
+	{
+		var line = Enumerable.Repeat('.', maxX + 1).ToArray();
+		for (int x = 0; x < maxX + 1; x++)
+		{
+			if (border!.Contains((x, y)))
+				line[x] = '#';
+			if (cur == (x, y))
+				line[x] = 'E';
+			var bliz = blizzards!.Where(b => (b.X, b.Y) == (x, y)).ToList();
+			if (bliz.Count > 1)
+			{
+				line[x] = '2';
+			}
+			else if (bliz.Count == 1)
+			{
+				line[x] = bliz.Single().Dir;
+			}
+		}
+		Console.WriteLine(new string(line));
+
+		//Console.WriteLine(new string(Enumerable.Range(0, maxX + 1)
+		//	.Select(x =>
+		//	  border!.Contains((x, y)) ? '#'
+		//	: cur == (x, y) ? 'E'
+		//	: blizzards!.Any(b => (b.X, b.Y) == (x, y))
+		//		? blizzards!.GroupBy(b => (b.X, b.Y)).Select(b => b.Count() > 1 ? b.Count().ToString()[0] : b.First().Dir).First()
+		//		: '.'
+		//	).ToArray()));
+	}
+}
+
