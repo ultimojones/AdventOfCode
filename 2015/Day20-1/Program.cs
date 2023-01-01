315749bd10e1332651de49cc64b65079697b37bf
Bert Lyons <ultimojones@gmail.com>
2023-01-01 12:28:34 +1100

    🤠

---
+++ 2015/Day20-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+
+// 861840
+
+for (int i = 1; i < 36000000; i += 1000)
+{
+    var house = Enumerable.Range(i, 1000).AsParallel()
+        .Select(i => (House: i, Presents: CalcPresents(i)))
+        .Where(i => i.Presents > 36000000);
+    if (house.Any())
+    {
+        Console.WriteLine(house.MinBy(h => h.House));
+        break;
+    }
+}
+
+int CalcPresents(int house)
+{
+	return Enumerable.Range(1, house).Sum(i => house % i == 0 ? i : 0) * 10;
+}
---
+++ 2015/Day20-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+// 3272728
+
+for (int i = 1; i < 36000000; i += 1000)
+{
+    var house = Enumerable.Range(i, 1000).AsParallel()
+        .Select(i => (House: i, Presents: CalcPresents(i)))
+        .Where(i => i.Presents > 36000000);
+    if (house.Any())
+    {
+        var min = house.MinBy(h => h.House);
+        Console.WriteLine(house.MinBy(h => h.House));
+        for (int j = min.House - 49; j <= min.House; j++)
+        {
+            if (min.House % j == 0)
+            {
+                Console.WriteLine($"{j} = {j * 11}");
+            }
+        }
+        break;
+    }
+}
+
+int CalcPresents(int house)
+{
+    var first = int.Max(1, house / 50);
+    return Enumerable.Range(first, house - first + 1).Sum(i => house % i == 0 ? i : 0) * 11;
+}
