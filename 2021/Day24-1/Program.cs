8e1192984514267822fbba2eade980ef3070967e
Bert Lyons <ultimojones@gmail.com>
2023-02-21 20:38:40 +1100

    🤠

--- 2021/Day24-1/Program.cs
+++ 2021/Day24-1/Program.cs
@@ -12,15 +12,17 @@ for (int i = 0; i < inputCommands.Length; i += 18)
 var states = new HashSet<(int pos, long digit, long zin, long zout)>();
 states.Add((14, 0, 0, 0));
 
-for (int d = 13; d >= 12; d--)
+for (int d = 13; d >= 0; d--)
 {
     foreach (var zout in states.Where(s => s.pos == d + 1).Select(s => s.zin).ToArray())
     {
         for (long w = 9; w > 0; w--)
         {
-            foreach (var zin in ReverseMonadDigit(d, w, zout))
+            var reverse = ReverseMonadDigit(d, w, zout).ToArray();
+            foreach (var zin in reverse)
             {
-                if (zout != ParseMonadDigit(d, w, zin))
+                var ztest = ParseMonadDigit(d, w, zin);
+                if (zout != ztest)
                     throw new Exception();
                 var state = (d, w, zin, zout);
                 states.Add(state);
@@ -30,20 +32,48 @@ for (int d = 13; d >= 12; d--)
     }
 }
 
-Console.WriteLine(states.Count(s => s.pos == 13 && s.zout == 0));
+
+var digits = new Stack<(int pos, long digit, long zin, long zout)>();
+foreach (var state in states.Where(s => s.pos == digits.Count && (digits.Count == 0 || s.zin == digits.Peek().zout)).OrderByDescending(s => s.digit))
+{
+    if (digits.Count == 13)
+    {
+        var digit = string.Concat(digits.Append(state).Select(d => d.digit));
+        Console.WriteLine(digit);
+    }
+}
+
+
 
 IEnumerable<long> ReverseMonadDigit(int d, long w, long z)
 {
     var parms = parameters![d];
 
-    yield return z * parms.DivZ + w - parms.AddX;
-
-    var zin = (int)(z - w - parms.AddY);
-    if (zin >= 0)
+    if (parms.DivZ == 26)
     {
-        foreach (var r in Enumerable.Range(zin, 25).Where(r => r > 26))
+        yield return z * parms.DivZ + w - parms.AddX;
+
+        var zin = z - w - parms.AddY;
+        if (zin % 26 == 0)
         {
-            yield return r;
+            foreach (var r in Enumerable.Range((int)zin, 25))
+            {
+                if (r % 26 + parms.AddX != w)
+                    yield return r;
+            }
+        }
+    }
+    else
+    {
+        if (z % 26 + parms.AddX == w)
+        {
+            yield return z;
+        }
+
+        var zin = z - w - parms.AddY;
+        if (zin % 26 == 0) 
+        {
+            yield return zin /= 26;
         }
     }
 }
@@ -58,7 +88,7 @@ long ParseMonadDigit(int d, long w, long z)
     x += parms.AddX;
     x = x == w ? 0 : 1;
     y = 25 * x + 1;
-    z = z * y;
+    z *= y;
     y = w;
     y += parms.AddY;
     y *= x;
